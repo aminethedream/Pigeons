463f20f0b0d30be0c5f1f487b03895f039573032
Amine TheDream <atourki@gmail.com>
2014-01-17 23:50:51 -0500

    add code

--- pigeon.rb
+++ pigeon.rb
@@ -17,3 +17,14 @@ pigeon_data = {
   }
 }
  
+ Pigeon_list = {}
+ pigeon_data.each_pair do |key, value|
+  value.each do |attribute, array|
+    array.each do |name|
+      Pigeon_list[name]={} if Pigeon_list[name].nil?
+      Pigeon_list[name][:color] = [] if Pigeon_list[name][:color].nil?
+      key == :color?  
+      Pigeon_list[name][key] << attribute : Pigeon_list[name][key] = attribute
+    end
+  end
+end
